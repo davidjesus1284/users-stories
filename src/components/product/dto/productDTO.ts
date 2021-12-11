39b66edef5a15c765f509e98bca1242feb0cbddb
jdescalante <jdescalante1416@gmail.com>
2021-12-11 12:25:40 -0500

    Creacion de config para swagger y modificación en consulta paginada

--- src/components/product/dto/productDTO.ts
+++ src/components/product/dto/productDTO.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class ProductDTO {
+    @ApiProperty()
     name: string;
+    @ApiProperty()
     sku: number;
+    @ApiProperty()
     price: number;
+    @ApiProperty()
     quantity: number;
 }
---
+++ src/utils/configSwagger.ts
@@ -0,0 +1,8 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const config = new DocumentBuilder()
+.setTitle('Service Auth')
+.setDescription('Pruebas de configuracion de api')
+.setVersion('1.0')
+.addTag('Rutas del servidor')
+.build();
