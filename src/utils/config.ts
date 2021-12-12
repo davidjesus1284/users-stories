746ee3a82f8a682f688cbde14209d3a4db3111f6
jdescalante <jdescalante1416@gmail.com>
2021-12-12 14:00:47 -0500

    Actualizado README.md

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,14 +1,14 @@
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
 
 export default (): SequelizeModuleOptions  => ({
-    dialect: 'mysql',
-    host: process.env.MYSQ_DB_HOST,
-    port: Number.parseInt(process.env.MYSQ_DB_PORT),
-    username: process.env.MYSQ_DB_USERNAME,
-    password: process.env.MYSQ_DB_PASSWORD,
-    database: process.env.MYSQ_DB_DATABASE,
+    dialect: 'mysql', // que tipo de base de datos se uso
+    host: process.env.MYSQ_DB_HOST, // ip del servidor
+    port: Number.parseInt(process.env.MYSQ_DB_PORT), // Puerto
+    username: process.env.MYSQ_DB_USERNAME, // usuario
+    password: process.env.MYSQ_DB_PASSWORD, // contraseña
+    database: process.env.MYSQ_DB_DATABASE, // base de datos
     models: [],
-    autoLoadModels: true,
-    synchronize: true
+    autoLoadModels: true, //Carga automaticamente los modelos de base de datos
+    synchronize: true // lo sincroniza con la DB
 });
 // Este archivo contiene la configuracion necesaria para que sequelize se conecte con la base de datos
